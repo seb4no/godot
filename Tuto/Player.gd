8c4a0fec1054a104c624775fd18c347713e77918
seb4no <sebzagreb@gmail.com>
2024-06-10 20:51:13 +0200

    pong

---
+++ Tuto/Player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func _ready():
+	var screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
+	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
